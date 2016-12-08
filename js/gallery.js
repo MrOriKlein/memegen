c304d3bad27674d3d43c3ac8247f91eda96a21c4
Ori Klein <Ori Klein>
2016-12-08 15:15:05 +0200

    Building the gallery hive hexagon

---
+++ js/gallery.js
@@ -0,0 +1,110 @@
+var gPics = [
+    {   
+        id: 1,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/1.jpg',
+        keywords: [],
+    },
+    {   
+        id: 2,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/2.jpg',
+        keywords: [],
+    },
+    {   
+        id: 3,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/3.jpg',
+        keywords: [],
+    },
+    {   
+        id: 4,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/4.jpg',
+        keywords: [],
+    },
+    {   
+        id: 5,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/5.jpg',
+        keywords: [],
+    },
+    {   
+        id: 6,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/6.jpg',
+        keywords: [],
+    },
+    {   
+        id: 7,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/7.jpg',
+        keywords: [],
+    },
+    {   
+        id: 8,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/8.jpg',
+        keywords: [],
+    },
+    {   
+        id: 9,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/9.jpg',
+        keywords: [],
+    },
+    {   
+        id: 10,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/10.jpg',
+        keywords: [],
+    },
+    {   
+        id: 11,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/11.jpg',
+        keywords: [],
+    },
+    {   
+        id: 12,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/12.jpg',
+        keywords: [],
+    },
+    {   
+        id: 13,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/13.jpg',
+        keywords: [],
+    },
+    {   
+        id: 14,
+        title: '',
+        desc: '',
+        url: '../assets/gfx/gallery/14.jpg',
+        keywords: []
+    }
+]
+
+var elMeet;
+var elContent;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    elMeet = document.querySelector('#picTemplate');
+    elContent = document.querySelector('.gallery');
+});
+
+function renderGallery() 
